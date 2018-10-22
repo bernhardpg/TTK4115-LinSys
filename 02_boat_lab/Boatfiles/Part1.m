811af011389bca3a78a2e06c39be8d7955eb63cb
Aksel Danielsen <akselsd@stud.ntnu.no>
2018-10-22 17:04:34 +0200

    Finish 5.1b

--- 02_boat_lab/Boatfiles/Part1.m
+++ 02_boat_lab/Boatfiles/Part1.m
@@ -11,4 +11,22 @@ C = [0 0 1 0 0];
 
 D = 0;
 
-[num, den] = ss2tf(A, B, C, D)
+[num, den] = ss2tf(A, B, C, D);
+
+h1 = 29.3575;
+h2 = 8.315;
+w1 = 0.005;
+w2 = 0.05;
+
+syms K_ T_
+eqs = [
+    h1 == K_/sqrt(T_^2*w1^4 + w1^2),
+    h2 == K_/sqrt(T_^2*w2^4 + w2^2)
+    ];
+
+[solK, solT] = solve(eqs, [K_ T_]);
+solK = double(solK);
+solT = double(solT);
+
+K = solK(1);
+T = solT(2);
