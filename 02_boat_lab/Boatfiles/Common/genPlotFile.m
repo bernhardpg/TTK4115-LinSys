fd4295a6a655fff414341fe862aa59f5ca6f4cae
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2018-11-06 13:42:19 +0100

    Generalize genPlotFile function

--- 02_boat_lab/Boatfiles/Common/genPlotFile.m
+++ 02_boat_lab/Boatfiles/Common/genPlotFile.m
@@ -1,12 +1,16 @@
-function [] = genPlotFile(prefix, titleString, yString, t, reference, state, stateLegend)
+function [] = genPlotFile(prefix, titleString, xLabel, yLabel, xArray, plot1, plot2, plot1Legend, plot2Legend)
     fig = figure;
     title(titleString);
-    ylabel(yString);
-    xlabel('Time [s]');
+    ylabel(yLabel);
+    xlabel(xLabel);
     hold on;
-    plot(t, state, 'b');
-    plot(t, reference, 'r');
-    legend({'Measured', stateLegend}, 'Location','southeast');
+    plot(xArray, plot1, 'b');
+    
+    if size(plot2, 2) ~= 0
+        plot(xArray, plot2, 'r');
+    end
+    
+    legend({plot1Legend, plot2Legend}, 'Location','southeast');
     hold off;
     filename = strrep(titleString, ' ', '_');
     filepath = strcat('../Common/figures/', prefix, '_', filename);
